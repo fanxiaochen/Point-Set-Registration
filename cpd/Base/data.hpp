e37c5c85ad3740c60025d764ba7d4fff87a63da8
fanxiaochen <fan.daybreak@gmail.com>
2014-10-11 13:36:48 +0800

    initialization and e-step

--- cpd/Base/data.hpp
+++ cpd/Base/data.hpp
@@ -8,12 +8,37 @@ namespace cpd
 {
 	typedef enum {EMPTY, RIGID, AFFINE, NONRIGID} RegType;
 
-	template <class T>
+	typedef float value_type;
+
+	template <typename T, int M>
 	void getInputData(const std::string& model_file, const std::string& data_file, 
-		T& model, T& data)
+		Matrix2<T, M>& model, Matrix2<T, M>& data)
 	{
 		// read files and fill the model and data matrices
+
 	}
+
+	template <typename T, int M>
+	void getInputData(const std::string& model_file, const std::string& data_file, 
+		Matrix3<T, M>& model, Matrix3<T, M>& data)
+	{
+		// read files and fill the model and data matrices
+
+	}
+
+	class Test
+	{
+	public:
+		void print(){std::cout << "ok" << std::endl;}
+	};
+
+	template <class T>
+	class MyTemp
+	{
+	public:
+		void test(){t_.printf();};
+		T t_;
+	};
 }
 
 #endif 
--- cpd/Core/cpd_rigid.hpp
+++ cpd/Core/cpd_rigid.hpp
@@ -1,6 +1,6 @@
 #ifndef CPD_RIGID_HPP
 #define CPD_RIGID_HPP
-
+#include <cmath>
 #include "Core/cpd_base.hpp"
 #include "Base/parameters.hpp"
 
@@ -14,7 +14,14 @@ namespace cpd
 		virtual ~CPDRigid();
 		void apply();
 	private:
-		RigidParas _paras;
+		value_type computeGaussianExp(size_t m, size_t n);
+		void e_step();
+		void m_step();
+	private:
+		RigidParas	_paras;
+		int			_dim;
+		value_type	_w;
+		Matrix		_corres;
 	};
 }
 
@@ -46,23 +53,67 @@ namespace cpd
 		}
 
 		// initialization
-		const int dim = model_cols;
-		_paras._R = Matrix::Identity(dim, dim);
-		_paras._t = Vector::Zero(dim, 1);
+		_dim = model_cols;
+		_paras._R = Matrix::Identity(_dim, _dim);
+		_paras._t = Vector::Zero(_dim, 1);
 		_paras._s = 1;
 
 		value_type sigma_sum = 0;
-		for (size_t i = 0; i < model_rows; i ++)
+		for (size_t m = 0; m < model_rows; m ++)
 		{
-			Vector model_row = _model->row(i);
-			for (size_t j = 0; j < data_rows; j ++)
+			Vector model_row = _model->row(m);
+			for (size_t n = 0; n < data_rows; n ++)
 			{
-				Vector data_row = _data->row(j);
+				Vector data_row = _data->row(n);
 				sigma_sum += Vector(model_row - data_row).squaredNorm();
 			}
 		}
 		_paras._squared_sigma = sigma_sum / (dim*model_rows*data_rows);
 
+		_w = 0.5; // w means?
+
+
+		//E-step
+
+	}
+
+	template <class T>
+	void CPDRigid<T>::computeGaussianExp(size_t m, size_t n)
+	{
+		Vector vec = _paras._s*_paras._R*_model->row(m) - _data->row(n);
+		value_type g_exp = exp(-vec.squaredNorm()/(2*_paras._squared_sigma));
+		return g_exp;
+	}
+
+	template <class T>
+	void CPDRigid<T>::e_step()
+	{
+		size_t model_rows = _model->rows();
+		size_t data_rows = _data->rows();
+		_corres.setZero(model_rows, data_rows);
+
+		for (size_t n = 0; n < data_rows; n ++)
+		{
+			std::vector<value_type> t_exp;
+			value_type sum_exp = 0;
+			value_type c = pow((2*PI*_paras._squared_sigma), _dim/2) * (_w/(1-_w)) * (model_rows/data_rows);
+			for (size_t m = 0; m < model_rows; m ++)
+			{
+				value_type m_exp = computeGaussianExp(m, n);
+				t_exp.push_back(m_exp);
+				sum_exp += m_exp;
+			}
+
+			for (size_t m = 0; m < model_rows; m ++)
+			{
+				_corres(m, n) = t_exp.at(m) / (sum_exp + c);
+			}
+		}
+	}
+
+	template <class T>
+	void CPDRigid<T>::m_step()
+	{
 
 	}
 }
